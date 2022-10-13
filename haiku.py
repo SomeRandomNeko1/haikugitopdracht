ddf8c83ff4339ce70e939c69f9f7d7d2a6f254aa
SomeRandomNeko1 <114647669+SomeRandomNeko1@users.noreply.github.com>
2022-10-13 14:58:29 +0200

    eerste haiku

--- haiku.py
+++ haiku.py
@@ -1,7 +1,23 @@
 
 
 #plak hier de haiku functions onder elkaar
+def Haiku1(): 
+    print("haiku by:","Li Hong") 
+    print("Niet schelden") 
+    print("Niet rennen in de gang") 
+    print("Niet mensen steken")
 
+def Haiku2(): 
+    print("haiku by:","AUTHOR") 
+    print("REGEL1") 
+    print("REGEL2") 
+    print("REGEL3")
+
+def Haiku3(): 
+    print("haiku by:","AUTHOR") 
+    print("REGEL1") 
+    print("REGEL2") 
+    print("REGEL3")
 
 
 
